df84a3f616e8bf2e6ba08e1bbb3dffcedde2dfaa
c3javier <c3javier@b306f97d-f658-4d3b-b6ad-734e435ea2f6>
2015-03-13 16:48:28 +0000

    Tested on cdf and returned validate.c back to normal

--- lab8/validate.c
+++ lab8/validate.c
@@ -15,7 +15,7 @@ The program exits with a value of 0 if the user id and password match,
 int main(void){
     int n, user_length;
     char userid[30];
-    char password[11];
+    char password[10];
     
     if((n = read(STDIN_FILENO, userid, 10)) == -1) {
         perror("read");
